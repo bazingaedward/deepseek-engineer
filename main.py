4ff5f67e834aaca062ee5259cd7ef2d5e197f2d0
kaixiang.qiu <kaixiang.qiu@guwave.com>
2025-04-16 17:21:55 +0800

    test: 基于openai完成跑过九宫格游戏，测试完全通过

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import getpass 
 
 if __name__ == "__main__":
-    # app()
+    app()
     # ai = ChatOpenAI(
     #     model='gpt-4o',
     #     temperature=0.1,
@@ -13,18 +13,3 @@ if __name__ == "__main__":
     #     # callbacks=[StreamingStdOutCallbackHandler()],
     # )
   
-
-    if not os.environ.get("OPENAI_API_KEY"):
-      os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
-
-    from langchain.chat_models import init_chat_model
-
-    model = init_chat_model("gpt-4o-mini", model_provider="openai")
-    from langchain_core.messages import HumanMessage, SystemMessage
-
-    messages = [
-        SystemMessage("Translate the following from English into Italian"),
-        HumanMessage("hi!"),
-    ]
-
-    model.invoke(messages)
